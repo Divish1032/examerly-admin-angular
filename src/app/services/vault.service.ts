1b294dd2aa3b02d9f74f3f7b5b95e45855b7b204
Divish1032 <kumar.divansh1996@gmail.com>
2020-05-14 01:36:54 +0530

    F

--- src/app/services/vault.service.ts
+++ src/app/services/vault.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class VaultService {
   apiDomain : String
   constructor() {
-    this.apiDomain = "http://ec2-15-206-153-117.ap-south-1.compute.amazonaws.com:3000"
+    this.apiDomain = "http://ec2-13-233-34-80.ap-south-1.compute.amazonaws.com:3000"
     //this.apiDomain = "//localhost:3000"
     
    }
